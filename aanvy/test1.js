d7fce36811f3837b5537728bd6af9be660e334ff
srikanthsiddatam <siddatamsrikanth7@gmail.com>
2023-10-10 20:04:04 +0530

    html code

--- aanvy/test1.js
+++ aanvy/test1.js
@@ -1,4 +1,19 @@
-function sri(){
-    var f=document.getElementsByClassName(task)
-    alert(f)
+// var s='{"name":"srikanth" , "roll":"12345" }'
+//  sa=JSON.parse(s)
+// console.log(sa)
+// var d={"name":"rama","roll":12345}
+// da=JSON.stringify(d)
+// console.log(da)
+const request=new XMLHttpRequest();
+request.open("GET","https://jsonplaceholder.typicode.com/users");
+ request.send();
+ request.onload = ( )=>{
+
+if(request.status === 200){
+    console.log(JSON.parse(request.response))
+
 }
+else{
+    console.log(`error ${request.status}`);
+}
+ }
